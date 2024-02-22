20928b5e3e1f1acb3d95ae1a75d2c4fd2c0ad5d9
Zak Sharp <zsharp@greatlakessolutions.com>
2024-02-22 09:53:37 -0600

    Update program contents to genearte metric

--- metric-instr/Program.cs
+++ metric-instr/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Diagnostics.Metrics;
+using System.Threading;
 
 namespace metric_instr
 {
     internal class Program
     {
+        static Meter s_meter = new Meter("HatCo.HatStore", "1.0.0");
+        static Counter<int> s_hatsSold = s_meter.CreateCounter<int>("hats-sold");
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var rand = new Random();
+            Console.WriteLine("Press any key to exit");
+            while (!Console.KeyAvailable)
+            {
+                //// Simulate hat selling transactions.
+                Thread.Sleep(rand.Next(100, 2500));
+                s_hatsSold.Add(rand.Next(0, 1000));
+            }
         }
     }
 }
