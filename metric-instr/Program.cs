d0984ae0f2e68b584f02db2dd0931896590fe423
Zak Sharp <zsharp@greatlakessolutions.com>
2024-02-22 09:50:39 -0600

    Add project files.

---
+++ metric-instr/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace metric_instr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
